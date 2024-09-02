9a41438718be23cf45998d9aff60157094ec0ec9
musmanirfan <usmanmala356@gmail.com>
2024-09-03 01:04:31 +0500

    add mui in project

---
+++ src/app/header.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='bg-[#322869] w-full'>
+        <h1 className='text-2xl text-white py-4 px-10'>Usman</h1>
+        <Image src={} alt='' height={} width={}/>
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,24 @@
 import { useEffect } from "react";
 import { useProductContext } from "./context/productContext";
 import Image from "next/image";
+import Header from "./header";
 
 
 
 export default function Home() {
   const { products, fetchProducts } = useProductContext()!;
-  // useEffect(() => console.log(products),
-  //   [])
   return (
     <>
+    <Header />
       <div className="grid grid-cols-3 gap-6 px-10 pt-5">
         {
           products.map((product) => (
-            <div key={product?.id} className=" border border-black rounded-lg">
+            <div key={product?.id} className=" border border-black rounded-lg overflow-hidden">
               <Image className="!object-contain !w-full h-44 border-b-[1px] border-black" alt="h" width={100} height={50} src={product?.image} />
               <h1 className="text-2xl font-semibold pl-5 pt-2">{product?.name}</h1>
               <h1 className="text-xl pl-5"> {product?.category}</h1>
               <h1 className="text-xl pl-5 pb-2">{product?.price}</h1>
+              <button className="w-full border border-t-black hover:bg-black hover:text-white transition-all py-2 duration-300">Add to Cart</button>
             </div>
           ))
         }
