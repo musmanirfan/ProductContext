b3143f292567395a66aa0c4d08fc291697532d3c
musmanirfan <usmanmala356@gmail.com>
2024-09-02 23:18:27 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,13 +11,13 @@ export default function Home() {
   //   [])
   return (
     <>
-      <div>
+      <div className="grid grid-cols-3">
         {
-          products.map((product)=>(
+          products.map((product) => (
             <div key={product?.id}>
-              <Image alt="h" width={100} height={50} src={product?.image} />
-              <h1>{product?.name}</h1>
-              <h1>{product?.category}</h1>
+              <Image className="!object-contain !w-full h-44" alt="h" width={100} height={50} src={product?.image} />
+              <h1 className="text-2xl font-semibold">{product?.name}</h1>
+              <h1> {product?.category}</h1>
               <h1>{product?.price}</h1>
             </div>
           ))
