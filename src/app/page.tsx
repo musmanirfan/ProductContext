0fb778d1745900af631d7b69fb48fb1b595a627d
musmanirfan <usmanmala356@gmail.com>
2024-09-03 00:52:28 +0500

    some styling added in cart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,14 +11,14 @@ export default function Home() {
   //   [])
   return (
     <>
-      <div className="grid grid-cols-3">
+      <div className="grid grid-cols-3 gap-6 px-10 pt-5">
         {
           products.map((product) => (
-            <div key={product?.id}>
-              <Image className="!object-contain !w-full h-44" alt="h" width={100} height={50} src={product?.image} />
-              <h1 className="text-2xl font-semibold">{product?.name}</h1>
-              <h1> {product?.category}</h1>
-              <h1>{product?.price}</h1>
+            <div key={product?.id} className=" border border-black rounded-lg">
+              <Image className="!object-contain !w-full h-44 border-b-[1px] border-black" alt="h" width={100} height={50} src={product?.image} />
+              <h1 className="text-2xl font-semibold pl-5 pt-2">{product?.name}</h1>
+              <h1 className="text-xl pl-5"> {product?.category}</h1>
+              <h1 className="text-xl pl-5 pb-2">{product?.price}</h1>
             </div>
           ))
         }
