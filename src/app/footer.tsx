b96de1e58df2aa2fb28a8f7204a201cf578331a4
musmanirfan <usmanmala356@gmail.com>
2024-09-04 02:20:46 +0500

    add some more styling

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <div className='w-full bg-[#322869] py-4 text-xl text-white text-center fixed bottom-0 left-0'>Created by <Favorite />
+        <div className='w-full bg-[#322869] py-4 text-xl text-white text-center '>Created by <Favorite />
             Muhammad Usman
         </div>
     )
