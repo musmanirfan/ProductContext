a9ccc996cf97601d5fc13f148a36c2bcf2c416d5
musmanirfan <usmanmala356@gmail.com>
2024-09-04 21:46:18 +0500

    Products convert in seperate component

---
+++ src/app/product.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image'
+import React from 'react'
+import { useProductContext } from './context/productContext'
+
+export default function Product() {
+
+    const {products, addToCart} = useProductContext()!;
+    return (
+        <div>
+            <div className="text-center mb-8 pt-32 px-10">
+                <h1 className="lg:text-6xl text-2xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-[#4ADE80] via-[#8e8ce5] to-[#1a291f]">Our Exclusive Product Collection</h1>
+                <p className="lg:text-xl text-gray-600 lg:mt-8 mt-2 mb-16 mx-auto lg:w-[80%]">
+                    {`Discover our wide range of high-quality products carefully curated to meet your needs. Whether you're looking for the latest technology or everyday essentials, we have something for everyone.`}
+                </p>
+            </div>
+            <div className="grid grid-cols-2 lg:grid-cols-4 lg:gap-6 lg:px-10 pb-16">
+                {
+                    products.map((product) => (
+                        <div key={product?.id} className="shadow-xl rounded-lg overflow-hidden p-5 flex flex-col items-center gap-2">
+                            <Image className="!object-contain !w-full h-44" alt="h" width={100} height={50} src={product?.image} />
+                            <h1 className="text-2xl font-semibold lg:pt-2">{product?.name}</h1>
+                            <h1 className="text-xl pb-2">${product?.price}</h1>
+                            <h1 className="text-md">Catogery: {product?.category}</h1>
+                            <button className="w-full border border-black rounded-full hover:bg-black hover:text-white transition-all py-2 duration-300" onClick={() => addToCart(product)}>Add to Cart</button>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
