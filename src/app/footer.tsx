68d097229d2b733f813733bfcbeaa17ca27628c4
musmanirfan <usmanmala356@gmail.com>
2024-09-04 21:59:32 +0500

    some changes in footer

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Footer() {
     return (
         <div className='w-full bg-[#322869] py-4 text-xl text-white text-center '>Created by <Favorite />
-            Muhammad Usman
+             Muhammad Usman
         </div>
     )
 }
